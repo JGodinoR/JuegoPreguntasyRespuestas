6ebe3373b007f732d5987e2cd03716ba91eddf04
PalomaLG91 <152387452+PalomaLG91@users.noreply.github.com>
2023-12-05 14:39:47 +0100

    4.0

--- src/App.java
+++ src/App.java
@@ -7,12 +7,22 @@ public class App {
         Random rd=new Random();
 
         String [] preguntas=new String [15];
+
         preguntas [0]="¿Cuanto es 2+2?";
         preguntas [1]="¿Cuantos lados tiene un cuadrado?";
         preguntas [2]="¿Cuantos lados tiene un triangulo?";
         preguntas [3]="¿Cuantas patas tiene un perro?";
         preguntas [4]="¿Cuantas ruedas tiene una bicicleta?";
+        preguntas [5]="¿Cuantos ojos tiene una mosca?";
+        preguntas [6]="¿Cual es la capital de España?";
+        preguntas [7]="¿Como se llaman los triangulos que tienen sus tres lados iguales?";
+        preguntas [8]="¿Cuantos meses tiene un año?";
+        preguntas [9]="¿Cuantos dias tiene una semana?";
+
+      
         System.out.println("Bienvenido a nuestro juego de preguntas y respuestas");
+
+
 
     }
 }
